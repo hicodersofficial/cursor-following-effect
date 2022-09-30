330e29e0653ece1162e6948979e88db27891811f
HiCoders <hicodersofficial@gmail.com>
2022-09-30 17:09:37 +0530

    refactored: ♻️ remove dead code

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 const eye1 = document.querySelector("body > svg > path:nth-child(4)");
 const eye2 = document.querySelector("body > svg > path:nth-child(2)");
 
-const eye = document.querySelector("body > svg > path:nth-child(7)");
 document.addEventListener("mousemove", handleMousemove);
 
 let factor = 23;
 
 function handleMousemove(e) {
-  const eyeRec = eye.getBoundingClientRect();
   let x, y;
   const widthMid = window.innerWidth / 2;
   const heightMid = window.innerHeight / 2;
